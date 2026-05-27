b6d503a265e477a1f407808fc46f9ee3303c7b32
HenriquePoltronieri <henrique.poltronieri2008@gmail.com>
2026-05-27 11:54:30 -0300

    atualização revisão

--- revisao_flutter/revisao/lib/pages/tela_revisao.dart
+++ revisao_flutter/revisao/lib/pages/tela_revisao.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
-class TelaRevisao extends StatefulWidget { // Mudar de estado
+class TelaRevisao extends StatefulWidget {  // Mudar de estado
   const TelaRevisao({super.key});
 
-  @override 
-  State<TelaRevisao> createState() => _TelaRevisaoState();
-  //Onde fica os dados
+  @override
+  State<TelaRevisao> createState() => _TelaRevisaoState(); //Onde fica os dados
 }
 
 class _TelaRevisaoState extends State<TelaRevisao> { //A tela que vai mudar de estado
@@ -14,21 +13,77 @@ class _TelaRevisaoState extends State<TelaRevisao> { //A tela que vai mudar de e
   @override
   Widget build(BuildContext context) {
     return Scaffold( //Corpo da aplicação
-  appBar: AppBar( //Cabeçalho
-    title: const Text("Revisão Flutter"),
+
+
+      appBar: AppBar( //Cabeçalho
+        title: const Text("Revisão Flutter"),
       ),
 
-      body: const Center ( //Centraliza o conteúdo dentro dele
-        child: Text("Conteúdo da tela"), 
+
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column( 
+            mainAxisAlignment: MainAxisAlignment.center, // Centraliza verticalmente
+            crossAxisAlignment: CrossAxisAlignment.center, // Centraliza horizontalmente
+
+            children: [
+              const Icon(
+                Icons.phone_android,
+                size: 80,
+                color: Colors.blue,
+              ),
+              const SizedBox(height: 20),
+
+              const Text(
+                'Revisão Flutter',
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 1
+                ),
+              ),
+              const SizedBox(height: 10),
+
+              const Text(
+                'Vamos revisar widgets,layout, estilos, botões, estado e navegação.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16
+                ),
+              ),
+              const SizedBox(height: 20,),
+
+              Container(
+                width: 320,
+                padding: const EdgeInsets.all(16),
+                margin: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.blue.shade50,
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                child: const Text(
+                  'O container funciona como uma caixa. Ele pode ter largura, padding, margin, cor e bordas.',
+                  textAlign: TextAlign.center,
+                ),
+
+              ),
+            ],
+          ),
+        ),
       ),
+
+
 
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           setState(() {
             mensagem = "Você clicou no botão flutuante";
-          });
-        }
-        ),
+          }
+        );
+      },
+  child: const Icon(Icons.add), // Adiciona o ícone de "+" aqui
+),
     );
   }
 }
